d27b40f4fdf40e86037a931ae60b217a18074062
Niharika Challuri <challuriniharika@gmail.com>
2026-04-17 10:38:50 +0530

    fixed static config

--- resume_creator/settings.py
+++ resume_creator/settings.py
@@ -23,9 +23,9 @@ TEMPLATES_DIR=BASE_DIR/'templates'
 SECRET_KEY = 'django-insecure-snhn@0uf4&t!dd2icxy(d6d8+ljwek+s%&$0=pc#i+g@(d8&6o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['resume-creator-66sd.onrender.com']
 
 
 # Application definition
@@ -143,4 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
