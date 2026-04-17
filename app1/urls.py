1cf8637ec094b9bce2a72bf70ed8058153f77ac0
Niharika Challuri <challuriniharika@gmail.com>
2026-04-17 11:06:40 +0530

    removed xhtml2pdf and fixed deployment

--- app1/urls.py
+++ app1/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.create_resume, name='create'),
     path('list/', views.resume_list, name='resume_list'),
-    path('download/<int:id>/', views.download_resume, name='download_resume'),
+    path('resume/<int:id>/', views.resume_detail, name='resume_detail'),
     path('edit/<int:id>/', views.update_resume, name='update_resume'),
     path('delete/<int:id>/', views.delete_resume, name='delete_resume'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from.forms import ResumeForm
 from .models import Resume
 
-from django.http import HttpResponse
-from django.template.loader import get_template
-from xhtml2pdf import pisa
+
 
 def create_resume(request):
     form = ResumeForm(request.POST or None)
@@ -17,18 +15,6 @@ def resume_list(request):
     resumes = Resume.objects.all()
     return render(request, 'list.html', {'resumes': resumes})
 
-def download_resume(request, id):
-    resume = Resume.objects.get(id=id)
-
-    template = get_template('resume_pdf.html')
-    html = template.render({'resume': resume})
-
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="resume.pdf"'
-
-    pisa.CreatePDF(html, dest=response)
-
-    return response
 
 def update_resume(request, id):
     resume = Resume.objects.get(id=id)
@@ -40,6 +26,11 @@ def update_resume(request, id):
         return redirect('resume_list')
     
     return render(request, 'form.html', {'form': form, 'is_edit': True})
+
+def resume_detail(request, id):
+    resume = Resume.objects.get(id=id)
+    return render(request, 'resume_detail.html', {'resume': resume})
+
 
 def delete_resume(request, id):
     resume = Resume.objects.get(id=id)
